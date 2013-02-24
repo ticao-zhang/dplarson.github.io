41f58661d9db12b843f734706583c08f7c9d71dc
David Larson <dplarson@ucsd.edu>
2013-02-24 03:01:29 -0800

    [fab] automate grabbing newest CV (.pdf)

--- fabfile.py
+++ fabfile.py
@@ -6,23 +6,46 @@ email: dplarson@ucsd.edu
 """
 
 from fabric.api import local
-from fabric.contrib.project import rsync_project
+import urllib
+
 
 def clean():
+    """Clean."""
     local('rm -rf deploy')
 
+
+def update_cv():
+    """Get current version of my CV."""
+    pdf_remote = "https://github.com/dplarson/CV/raw/master/David_Larson.pdf"
+    pdf_local = "deploy/media/David_Larson.pdf"
+    try:
+        urllib.urlretrieve(pdf_remote, pdf_local)
+    except:
+        print "Error: unable to download newest version of CV."
+
+
 def gen():
-	clean()
-	local('hyde gen')
+    """Generate website."""
+    clean()
+    local('hyde gen')
+    update_cv()
+
 
 def gen_prod():
+    """Generate production version of website."""
     clean()
     local('hyde gen -c prod.yaml')
+    update_cv()
+
 
 def serve():
-	gen()
-	local('hyde serve')
+    """Start local webserver."""
+    gen()
+    local('hyde serve')
+    update_cv()
+
 
 def push():
+    """Upload website to UCSD server."""
     gen_prod()
     local('rsync -rav deploy/ dplarson@ieng6.ucsd.edu:/home/linux/ieng6/oce/60/dplarson/public_html/')
