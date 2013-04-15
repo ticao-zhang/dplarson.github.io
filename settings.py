0f03e20d4a41f7776690c3ca3bd321d9c4e7dbcc
David Larson <dplarson@ucsd.edu>
2013-04-14 23:58:32 -0700

    Start adding content.

--- settings.py
+++ settings.py
@@ -22,3 +22,16 @@ TAGS_SAVE_AS = False
 ARCHIVES_SAVE_AS = False
 CATEGORIES_SAVE_AS = False
 FEED_ALL_ATOM = None
+
+# Analytics
+GOOGLE_ANALYTICS_ID = 'UA-36350691-1'
+
+# Links
+URL_COIMBRA = 'http://coimbra.ucsd.edu'
+URL_IDEA = 'http://www.jacobsschool.ucsd.edu/student/scholars/'
+URL_LEADS = 'http://graduatedivision.ucmerced.edu/grad-prep-programs/uc-leads'
+URL_NERDS = 'http://ucberkeleynerds.com'
+URL_INNOVATE = 'https://eng.ucmerced.edu/innovatetogrow'
+
+URL_GITHUB = 'https://github.com/dplarson'
+URL_LINKEDIN = 'http://www.linkedin.com/pub/david-larson/65/334/934/'
